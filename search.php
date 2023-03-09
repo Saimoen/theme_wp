78a6ed5b5f0b76249e41cdcf4b735df7df1cc509
Grégory <greg.eres.07@hotmail.com>
2023-03-09 11:36:12 +1100

    22 finish

--- search.php
+++ search.php
@@ -1,5 +1,37 @@
 <?php get_header() ?>
 
+<form>
+  <fieldset>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Disabled input</label>
+      <input type="search" name="1" class="form-control" placeholder="Disabled input" value="<?= get_search_query() ?>" placeholder="Votre recherche">
+    </div>
+    <div class="mb-3">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" value="1" name="sponso" id="inlineFormCheck" <?= checked('1', get_query_var('sponso'))?> >
+        <label class="form-check-label" for="disabledFieldsetCheck">
+          Article sponsorisé seulement
+        </label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+</form>
+
+<!-- <form class="form-inline">
+  <div class="form-group mb-2">
+    <input type="search" name="s" readonly class="form-control-plaintext" value="<?= get_search_query() ?>" placeholder="Votre recherche">
+    <div class="form-check mb-2 mr-sm-2">
+        <input type="checkbox" class="form-check-input" value="1" id="inlineFormCheck">""
+        <label class="form-check-label" for="inlineFormCheck">
+            Article sponsorisé seulement
+        </label>
+    </div>
+</div>
+  
+  <button type="submit" class="btn btn-primary mb-2">Rechercher</button>
+</form> -->
+
 <h1 class="mb-4">Résultat pour votre recherche "<?= get_search_query() ?>"</h1>
 
 <?php if (have_posts()): ?>
